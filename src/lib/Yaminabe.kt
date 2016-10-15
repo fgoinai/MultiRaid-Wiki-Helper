d4803e7540f83a881ce785d41b64752f58d83855
fgoinai@gmail.com <fgoinai@gmail.com>
2016-10-16 02:44:43 +0900

    Added Copyright as GPL V3

--- src/lib/Yaminabe.kt
+++ src/lib/Yaminabe.kt
@@ -1,3 +1,16 @@
+/*
+ * File: Yaminabe.kt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3
+ * of the License.
+ *
+ * Project: MultiRaid Wiki Helper
+ * Author: FGO
+ * Copyright (c) 2016. FGO Production. All right reserved
+ */
+
 package lib
 
 import java.io.BufferedReader
