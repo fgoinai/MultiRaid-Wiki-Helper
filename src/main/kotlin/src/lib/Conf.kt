5c22360aa7c93cb0556ce1aa76c6decacf6bee42
fgoinai@gmail.com <fgoinai@gmail.com>
2017-04-10 00:50:07 +0900

    Change four angel tab characteristic, added user agent on http request to anti detection

---
+++ src/main/kotlin/src/lib/Conf.kt
@@ -0,0 +1,48 @@
+/*
+ * File: Conf.kt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3
+ * of the License.
+ *
+ * Project: MultiRaid Wiki Helper
+ * Author: FGO
+ * Copyright (c) 2016. FGO Production. All right reserved
+ */
+
+package src.lib
+
+import org.ini4j.Ini
+import java.io.File
+
+object Conf {
+    private val ini = Ini(File("conf.ini"))
+
+    fun getIntervalConf(lower: Long): Long {
+        var ret = ini.get("Other", "RenewInterval", Long::class.java)
+        if (ret <= lower) {
+            ret = lower
+        }
+        return ret
+    }
+
+    fun getUrlConf(): HashMap<String, String> {
+        val ret = HashMap<String, String>()
+        getIndex().forEach { ret.put(it, ini.get("URL", it)) }
+        return ret
+    }
+
+    fun getUrl(tag: String): String {
+        return ini.get("URL", tag)
+    }
+
+    private fun getIndex() = arrayOf(
+            "大巴",
+            "四天司",
+            "方陣HL",
+            "通常",
+            "召喚終突",
+            "6人HL"
+    )
+}
