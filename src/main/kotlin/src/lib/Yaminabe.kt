0eb911b1ef56d4980ca7dfb1470c3cd5fdea0f0d
fgoinai@gmail.com <fgoinai@gmail.com>
2017-04-15 22:41:36 +0900

    Kotlin 1.1 generator is buggy, replaced that with reader stream and extract codes into fun

--- src/main/kotlin/src/lib/Yaminabe.kt
+++ src/main/kotlin/src/lib/Yaminabe.kt
@@ -15,81 +15,77 @@ package src.lib
 
 import src.category.CatCommonFun.idPattern
 import src.category.CatCommonFun.nonWordPattern
-import src.category.CatCommonFun.spacing
 import src.category.CbUpdateList
 import src.category.ICategory
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
-import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.*
-import kotlin.coroutines.experimental.buildSequence
 import kotlin.system.measureTimeMillis
 
 class Yaminabe {
     private val maxListLen = 5
 
     fun getList(cat: ICategory, cb: CbUpdateList) {
-        val queue = ConcurrentLinkedQueue<String>()
+        val list = ArrayList<String>()
         val con = URL(cat.url).openConnection() as HttpURLConnection
         con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0") //Anti Detection
         val reader = InputStreamReader(con.inputStream, Charset.forName("EUC-JP")).buffered()
-        val buffer = buildSequence {
-            while (true) {
-                yield(java.lang.String(reader.readLine()?.toByteArray() ?: break, "UTF-8") as String)
-            }
-        }
 
         val time = measureTimeMillis {
-            try {
-                buffer.forEach {
-                    if (it.contains("<span class=\"comment_date\">")) {
-                        if (isTargetEmpty(it)) return
-                        val ret = cat.filter(it) ?: return
-
-                        while (queue.size >= maxListLen) {
-                            queue.sortedWith(
-                                    compareBy(
-                                            {
-                                                SimpleDateFormat("yyyy-MM-dd").parse(
-                                                        it.split(spacing)
-                                                                .filter { it.contains(Regex("\\d{4}-\\d{2}-\\d{2}")) }[0]
-                                                                .split(" ")[0]
-                                                )
-                                            },
-                                            {
-                                                SimpleDateFormat("HH:mm:ss").parse(
-                                                        it.split(spacing)
-                                                                .filter { it.contains(Regex("\\d{2}:\\d{2}:\\d{2}")) }[0]
-                                                                .split(" ")
-                                                                .filter { it.contains(Regex("\\d{2}:\\d{2}:\\d{2}")) }[0]
-                                                )
-                                            }
-                                    )
-                            )
-                            queue.poll()
+            reader.lines()
+                    .filter { encodingJpToUtf8(it).contains("<span class=\"comment_date\">") }
+                    .filter { !isTargetEmpty(encodingJpToUtf8(it)) }
+                    .forEach {
+                        val ret = cat.filter(encodingJpToUtf8(it))
+                        if (null != ret) {
+                            pushToQueue(list, ret)
                         }
-                        queue.offer(ret)
                     }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+        }
 
-            if (queue.isNotEmpty()) {
-                cb.update(queue.reversed() as ArrayList<String>)
-            } else {
-                cb.update(null)
-            }
+        if (list.isNotEmpty()) {
+            cb.update(list.reversed() as ArrayList<String>)
+        } else {
+            cb.update(null)
         }
         println("${javaClass.name} cost $time")
+    }
+
+    private fun pushToQueue(list: ArrayList<String>, src: String) {
+        while (list.size >= maxListLen) {
+//            list.sortedWith(
+//                    compareBy(
+//                            {
+//                                SimpleDateFormat("yyyy-MM-dd").parse(
+//                                        it.split(spacing)
+//                                                .filter { it.contains(Regex("\\d{4}-\\d{2}-\\d{2}")) }[0]
+//                                                .split(" ")[0]
+//                                )
+//                            },
+//                            {
+//                                SimpleDateFormat("HH:mm:ss").parse(
+//                                        it.split(spacing)
+//                                                .filter { it.contains(Regex("\\d{2}:\\d{2}:\\d{2}")) }[0]
+//                                                .split(" ")
+//                                                .filter { it.contains(Regex("\\d{2}:\\d{2}:\\d{2}")) }[0]
+//                                )
+//                            }
+//                    )
+//            )
+            list.remove(list[0])
+        }
+        list.add(src)
     }
 
     fun isTargetEmpty(src: String) : Boolean {
         return src.split(nonWordPattern)
                 .filter { it.matches(idPattern) }
                 .isEmpty()
+    }
+
+    fun encodingJpToUtf8(src: String): String {
+        return java.lang.String(src.toByteArray(), "UTF-8") as String
     }
 }
